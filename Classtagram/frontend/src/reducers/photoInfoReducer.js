ed066bf0693fa7b337515e7a359dc24f204c076f
sjho <sjh2177@gmail.com>
2019-06-18 00:58:38 +0900

    backend-frontend 연결

---
+++ Classtagram/frontend/src/reducers/photoInfoReducer.js
@@ -0,0 +1,32 @@
+import * as types from '../actions';
+import { initialState } from '../components/registerPage'
+import { takeEvery, all, take, put, call, fork } from 'redux-saga/effects'
+
+export default function(state = initialState, action) {
+  const response = action.response;
+
+  switch(action.type) {
+    case types.PHOTO_INFO_SUCCESS:
+      console.log(response);
+      return {response};
+
+    //	return Object.assign({}, state, response.info, {
+    //  		response: response
+    //	});
+    	// response.info = {
+     //    username: ...
+     //    password: ...
+     //    major: ...
+     //    ...
+     //  }
+     //  response.meta = {
+     //    success: ...
+     //    message: ...
+     //  }
+    case types.PHOTO_INFO_ERROR:
+      return {response};
+
+    default:
+      return state;
+  }
+};
